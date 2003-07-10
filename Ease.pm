894004d0878dba73bcac72e658fb2c8eab4a77ea
Alexander David P <alpo@cpan.org>
2003-07-10 00:52:31 +0000

    Import of ALPO/DBIx-Ease-0.04 from CPAN.
    gitpan-cpan-distribution: DBIx-Ease
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ALPO/DBIx-Ease-0.04.tar.gz
    gitpan-cpan-author:       ALPO
    gitpan-cpan-maturity:     released

--- Ease.pm
+++ Ease.pm
@@ -4,7 +4,9 @@ package DBIx::Ease;
 use strict;
 use DBI;
 use vars qw($VERSION);
-$VERSION = "0.02";
+$VERSION = "0.04";
+
+1;
 
 sub new {
   my $class = shift;
@@ -21,28 +23,66 @@ sub exec {
   my $sql = shift;
   my @sel;
   unless ($self->{'cons'}{$name}){
-  	unless ($self->{'drv'} ne "CSV"){
-  	  $self->{'cons'}{$name} = DBI->connect("DBI:CSV:f_dir=$name");
-  	}
+    unless ($self->{'drv'} ne "CSV"){
+      $self->{'cons'}{$name} = DBI->connect("DBI:CSV:f_dir=$name");
+    }
   }
-
   unless ($self->{'cons'}{$name}){return;}
   my $sth = $self->{'cons'}{$name}->prepare($sql);
   $sth->execute();
   if ($sql =~ m/^SELEC/i){
-  	while (my $row = $sth->fetch){push(@sel, @$row);}
-  	return @sel;
+    while (my $row = $sth->fetch){push(@sel, @$row);}
+    return @sel;
   }
 }
-
 
 sub disconnect_all {
   my $self = shift;
   my $e;
   my %kwas = %{$self->{'cons'}};
   foreach $e (keys %kwas){
-  	$self->{'cons'}{$e}->disconnect;
+    $self->{'cons'}{$e}->disconnect;
   }
 }
 
-1;
+
+=head1 NAME
+
+    DBIx::Ease - less-code DBI interactions
+
+=head1 SYNOPSIS
+
+    use DBIx::Ease;
+
+    my $csv_cons = new DBIx::Ease('CSV');
+
+    $csv_cons->exec(
+       "databasename",      # using CSV this is the path
+       "DELETE from somename where i=1");
+
+    my @results = $csv_cons->exec("databasename","SELECT ..");
+
+    $csv_cons->disconnect_all;
+
+=head1 DESCRIPTION
+
+    DBIx::Ease is intended to allow less-code DBI interactions. Version 0.04
+    uses DBD::CSV. exec() takes currently two arguments, the path 'f_dir'
+    and a SQL statement. exec() handles connects or reuse of connections,
+    prepare, execute and returns an array of all selected rows for SELECTs.
+
+    DBIx::Ease is currently in pre-alpha; rather a prototype.
+    
+=head1 INSTALLATION
+
+    Standard build/installation supported by ExtUtils::MakeMaker(3)...
+    perl Makefile.PL
+    make
+    make install
+
+=head1 AUTHOR
+
+    Alexander Pourmirzayouf <commerce_cgi@yahoo.com>
+
+=cut
+
